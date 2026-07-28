bff75ea593767137ca1fbbeb4977dfcbfd154b65
Djordje <djordjesipka94@gmail.com>
2026-07-28 21:57:15 +0200

    style: remove unused login styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-import { Inter, Archivo_Black, JetBrains_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter, Archivo_Black, JetBrains_Mono } from "next/font/google"
+import "./globals.css"
 
 const inter = Inter({
     subsets: ["latin"],
